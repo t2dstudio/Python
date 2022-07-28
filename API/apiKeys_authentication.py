a73f522f1365b138d587a5389983e344b46f99a5
Kehinde Adeoye <t2dstudio@yahoo.com>
2022-07-28 00:41:09 -0500

    Starting Api Auth

---
+++ API/apiKeys_authentication.py
@@ -0,0 +1,26 @@
+#.........API AUTHENTICATION.........................
+#How do you authentiate yourself
+#....API KEY............
+import requests
+api_key = "7d741f1ff96bdb5264e759aad9fbb9b8"
+api_url = "https://api.openweathermap.org/data/2.5/onecall"
+parameters = {
+    "lat":37.964252,
+    "lon": -91.831833,
+    "appid":api_key,
+    "exclude": "current,minutely,daily"
+}
+response = requests.get(api_url, params=parameters)
+response.raise_for_status()
+#print(response.status_code)
+weather_data = response.json()
+hourly_data = weather_data["hourly"]
+
+will_rain = False
+for hour_data in range(len(hourly_data)-35): # Use SLICE instaed for hour_data in hourly_data[:12]
+    condition_code = hourly_data[hour_data]["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring Umbrella")
--- Game12_Trivia_API/ui.py
+++ Game12_Trivia_API/ui.py
@@ -40,8 +40,14 @@ class Quiz:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz ")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
 
     def true_pressed(self):
